e15245f1197872dc51671cf52049930f27628e71
IvanGek <ivan.gongora_pm24303@cecyte.edu.mx>
2026-03-06 10:38:49 -0600

    feat: estructura inicial del proyecto.

---
+++ src/main/java/com/mycompany/pracgit_bscg/main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pracgit_bscg;
+
+/**
+ *
+ * @author Alumno
+ */
+
+
+   public class main{
+        public static void main(String[] args) {
+          System.out.println("--- Sistema de Gestión de Alumnos ---");
+// Aquí irán las mejoras
+   
+
+}
+}
+    
+
